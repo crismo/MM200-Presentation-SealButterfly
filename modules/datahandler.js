f3467388912de5972a5aa4e194b97eec218a52d0
kjetkr01 <kjetil2001@gmail.com>
2020-10-30 11:37:49 +0100

    oppdatering

--- modules/datahandler.js
+++ modules/datahandler.js
@@ -1,2 +1,21 @@
+const pg = require("pq");
 const dbCredentials = process.env.DATABASE_URL || require("../localenv");
 
+class StorageHandler {
+
+    constructor(credentials){
+        this.credentials = {
+            connectionString: credentials,
+            ssl: {
+                rejection: false
+            }
+        }
+    }
+
+    async insertUser(username, password){
+        console.log("insertUser");
+    }
+
+}
+
+module.exports = StorageHandler
