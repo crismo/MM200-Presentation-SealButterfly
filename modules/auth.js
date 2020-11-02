d88f429cc4b77511f7185011a131e37b2bd4d165
kjetkr01 <kjetil2001@gmail.com>
2020-11-02 12:41:27 +0100

    auth

--- modules/auth.js
+++ modules/auth.js
@@ -1,7 +1,20 @@
 const authenticator = (req, res,next) => {
 
+    console.log(req.headers.authorization);
+}
 
+    /*if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
+        return res.append("WWW-Authenticate", 'Basic realm="User Visible Realm", charset="UTF-8"').status(401).end()
+    }
 
+    const credentials = req.headers.authorization.split(' ')[1];
+    const [username, password] = Buffer.from(credentials, 'base64').toString('UTF-8').split(":");
+
+    const user = authenticate(username, password)
+    if (user) {
+        return res.status(403).end()
+    }
+    next();
 
 
 }
@@ -11,10 +24,9 @@ const authenticator = (req, res,next) => {
 
 
 function authenticate(username, password){
-
+    console.log(username + ":" + password);
 };
-
-
+*/
 
 
 module.exports = authenticator
