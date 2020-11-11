491add3ff2f7d74d60c4659fd6eb8c331e072983
Christian Simonsen <crismo@users.noreply.github.com>
2020-11-11 12:57:45 +0100

    Update sbToken.js
    forslag til enkelt token

---
+++ modules/sbToken.js
@@ -0,0 +1,25 @@
+const user = requier("./user.js");
+const crypto = require('crypto');
+const secret = process.env.hashSecret || require("../localenv").tokenSecret;
+
+function createToken(user){
+  
+  let body = {created:new Date().now(), user:JSON.stringify(user), validTo:"Dato i fremtiden"}
+  
+  let token  = crypto.createHmac('sha256', secret) // Bruk annen algoritme 
+            .update(body)
+            .digest('hex');
+  
+    return token;
+}
+
+function validateToken(token, user){
+
+    // omvendt av det som ligger i createToken?
+    // er token info === user info
+    // Er tokenet utløpt??
+}
+
+
+module.exports.create = createToken;
+module.exports.validate = validateToken;
