1eff6d708b52a51c07bc503ed1c2c6ad233ca9ce
Christian Simonsen <crismo@users.noreply.github.com>
2020-11-11 13:10:59 +0100

    Update auth.js
    Eksempel på auth med token

--- modules/auth.js
+++ modules/auth.js
@@ -1,30 +1,22 @@
 const user = require("./user");
+const validateToken = require("./sbToken").validate;
+
 const authenticator = async (req, res,next) => {
-    
 
-    if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
-        //return res.append("WWW-Authenticate", 'Basic realm="User Visible Realm", charset="UTF-8"').status(401).end(); //problem siden res blir ikke lest
-        return 401;
-    }
+    if(req.body.token != undefined){ // Altså har klient sendt med en verdi for et felt token?       
+        if(validateToken(req.body.token)){ // dersom token er gyldig
+            
+            // på dette tidspunktet så kan vi pakke ut token og lage user objekt. 
+            // let username = token.username;
+            // let psw = token.password;
+            // ......
+            // req.user = user(username,psw);
+            
+            next();
+        }
+    } 
 
-    const credentials = req.headers.authorization.split(' ')[1];
-    const [username, password] = Buffer.from(credentials, 'base64').toString('UTF-8').split(":");
-
-    const checkUser = new user(username, password);
-    const resp = await checkUser.login(); //response
-   
-    //console.log(resp);
-
-    req.user = checkUser;
-
-    //console.log(user)
-    if(!checkUser) {
-        return 403;
-    }
-    //next();
-
-    return resp;
-
+    res.status(403).end();
 }
 
 
